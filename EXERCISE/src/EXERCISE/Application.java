fa7ac3bf52202957d03cbe6c89afc1b6865c60b3
Blessings Wandji <120319884+Bledsings@users.noreply.github.com>
2023-10-18 02:06:36 +0100

    Add files via upload

--- EXERCISE/src/EXERCISE/Application.java
+++ EXERCISE/src/EXERCISE/Application.java
@@ -2,7 +2,7 @@ package EXERCISE;
 
 public class Application {
 	public static boolean exercise(int k, int[] numbers) {
-		for (int i = 0; i < numbers.length; i++) {
+		for (int i = 0; i < numbers.length;) {
 
 			if (numbers[i] + numbers[i + 1] == k) {
 
@@ -27,18 +27,27 @@ public class Application {
 			} else if (numbers[i + 2] + numbers[i + 3] == k) {
 
 				return true;
-			}
+			}else {
+				
+				return false;
 
-		}
+			}}
+		return false;
+
 		
-	return false;
+		
+		 
+		
+		
+		
+	
 
 	}
 	public static void main(String[] args) {
-		int[] numbers = { 10, 4, 3, 8 };
+		int[] numbers = { 10, 7, 3, 8 };
 		
 
-			System.out.println(exercise(13, numbers));
+			System.out.println(exercise(20, numbers));
 		
 
 	}
